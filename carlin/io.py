d54901de5bcb1b77073b6d02a4809c54311cbadd
mforets <marcelo.forets@imag.fr>
2017-04-04 08:21:45 +0200

    fix square bracket notation

--- carlin/io.py
+++ carlin/io.py
@@ -40,9 +40,9 @@ from sage.rings.all import CC
 from sage.rings.real_double import RDF
 from sage.rings.polynomial.polynomial_ring import polygens 
 
-#===============================================
+#==========================
 # Functions to load a model
-#===============================================
+#==========================
 
 def load_model(model_filename):
     r""" Read an input ODE system.
@@ -114,7 +114,7 @@ def get_Fj_from_model(model_filename=None, f=None, n=None, k=None):
         [f, n, k] = load_model(model_filename)
 
     # create the collection of sparse matrices Fj
-    F = [sp.dok_matrix((n,n^i), dtype=np.float64) for i in [1..k]]
+    F = [sp.dok_matrix((n,n^i), dtype=np.float64) for i in range(1, k+1)]
 
     # read the powers appearing in each monomial
     dictionary_f = [fi.dict() for fi in f];
