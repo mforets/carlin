4299e8efacfae5f20c9ba51ab804f9cc1fe99253
mforets <marcelo.forets@imag.fr>
2017-05-27 20:10:24 +0200

    update library

--- carlin/library.py
+++ carlin/library.py
@@ -1,6 +1,17 @@
 r"""
 Library of commonly used or famous polynomial ODE systems.
 
+The following functions are available:
+
+.. csv-table::
+    :class: contentstable
+    :widths: 30, 70
+    :delim: |
+
+    :func:`~vanderpol`            | `Van der Pol oscillator <https://en.wikipedia.org/wiki/Van_der_Pol_oscillator>`_
+    :func:`~scalar_cubic`         | A scalar ODE with a cubic term
+    :func:`~scalar_quadratic`     | A scalar ODE with a quadratic term
+
 AUTHOR:
 
 - Marcelo Forets (May 2017)
@@ -21,7 +32,7 @@ from sage.rings.all import RR, QQ
 from sage.rings.real_double import RDF
 from sage.rings.polynomial.polynomial_ring import polygens 
 
-def vanderpol(mu, omega):
+def vanderpol(mu=1, omega=1):
     r"""
     The Van der Pol oscillator is a non-conservative system with non-linear damping.
     
@@ -57,5 +68,72 @@ def vanderpol(mu, omega):
 
     # order of the ODE
     k = 3
-
     return f, n, k
+
+def scalar_cubic(a, b):
+    r"""
+    A scalar ODE with a cubic term.
+    
+    It is defined as:
+
+    .. MATH::
+
+        x'(t) = -ax(t) + bx(t)^3
+
+    where `a` and `b` are paremeters of the ODE.
+
+    EXAMPLES::
+
+        sage: from carlin.library import scalar_cubic
+        sage: scalar_cubic()
+        ([x0^3 - x0], 1, 3)
+
+    Compute the Carleman embedding truncated at order `N=4`::
+
+        sage: from carlin.transformation import get_Fj_from_model, truncated_matrix 
+        sage: Fj = get_Fj_from_model(*scalar_cubic())
+        sage: matrix(truncated_matrix(4, *Fj, input_format="Fj_matrices").toarray())
+        [-1.0  0.0  1.0  0.0]
+        [ 0.0 -2.0  0.0  2.0]
+        [ 0.0  0.0 -3.0  0.0]
+        [ 0.0  0.0  0.0 -4.0]
+    """
+    # define the vector of symbolic variables
+    x = polygens(QQ, ['x'+str(0)])
+    f = [None] * 1
+    f[0] = - a * x[0] + b * x[0]**3
+    return f, 1, 3
+
+def scalar_quadratic(a, b):
+    r"""
+    A scalar ODE with a quadratic term.
+
+    It is defined as:
+
+    .. MATH::
+
+        x'(t) = ax(t) + bx(t)^2
+
+    where `a` and `b` are paremeters of the ODE.
+
+    EXAMPLES::
+
+        sage: from carlin.library import scalar_cubic
+        sage: scalar_quadratic()
+        ([x0^3 - x0], 1, 3)
+
+    Compute the Carleman embedding truncated at order `N=4`::
+
+        sage: from carlin.transformation import get_Fj_from_model, truncated_matrix 
+        sage: Fj = get_Fj_from_model(*scalar_cubic())
+        sage: matrix(truncated_matrix(4, *Fj, input_format="Fj_matrices").toarray())
+        [-1.0  0.0  1.0  0.0]
+        [ 0.0 -2.0  0.0  2.0]
+        [ 0.0  0.0 -3.0  0.0]
+        [ 0.0  0.0  0.0 -4.0]
+    """
+    # define the vector of symbolic variables
+    x = polygens(QQ, ['x'+str(0)])
+    f = [None] * 1
+    f[0] =  a * x[0] + b * x[0]**2
+    return f, 1, 2
